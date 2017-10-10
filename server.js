34646339debfab4de0d8fe9d7e7dc7b61b4bec04
kmboyle <kevin.michael.boyle@gmail.com>
2017-10-10 09:47:37 -0400

    adding heroku

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
-const xphb = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cheerio = require("cheerio");
 const request = require("request");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function() {
+
+    console.log("Server listening on port " + PORT);
+});
