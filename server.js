8eeaef9105b5675b3679152c1061634059cedd1c
kmboyle <kevin.michael.boyle@gmail.com>
2017-10-10 09:37:05 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const xphb = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cheerio = require("cheerio");
+const request = require("request");
